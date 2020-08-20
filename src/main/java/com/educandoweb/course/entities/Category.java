131b5b5b3530fa3028c3797a3336ec9f16a701e7
JulioMaiaB <julio_maia1@hotmail.com>
2020-08-20 17:26:58 -0300

    Product

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -1,20 +1,21 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-@AllArgsConstructor
-@Data
+@EqualsAndHashCode
 @Entity
 @Table(name = "tb_category")
 public class Category implements Serializable {
@@ -24,5 +25,34 @@ public class Category implements Serializable {
 	private Long id;
 	private String name;
 	
+//	@ManyToMany(mappedBy = "categories")
+	@Transient
+	Set<Product> products = new HashSet<>();
+
+	public Category(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
+	}
+
 	
 }
