42d5e3096b3e43032d71ffbe9e79a7204c8db87d
JulioMaiaB <julio_maia1@hotmail.com>
2020-08-21 15:52:30 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -8,8 +8,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -18,19 +22,21 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable{
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String description;
 	private Double price;
-	private String imgUrl; //?
+	private String imgUrl;
 	
-//	@ManyToMany(mappedBy = "products")
-	@Transient
-	private Set<Category> categories = new HashSet<>(); //?
+	@JsonIgnore
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>();
 
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		this.id = id;
@@ -83,6 +89,5 @@ public class Product implements Serializable{
 	public Set<Category> getCategories() {
 		return categories;
 	}
-	
-	
+
 }
